54885da5da15f2ba9f42cf7da2d7a1df4a73c7b8
minawoww <mina.medhat100@gmail.com>
2019-10-02 14:58:41 +0200

    dev3

--- ICU.c
+++ ICU.c
@@ -1,14 +1,15 @@
 
 
 #include "ICU.h"
-#include "TIMER.h"
 
 
 // TIMER0_ov_Count
 
 //float32 G_Pulse_time = 0;
 float32 G_Pulse_time = 0;
-	static uint8 edge_flag = 0;
+static uint8 edge_flag = 0;
+uint8 G_OV_Counts = 0;
+
 void (*G_CallBackFunction)(void) = ( (void*) 0);
 
 
@@ -17,26 +18,29 @@ void ICU_init()
 
 	INTP_vidInit();
 	INTP0_vidSetCallBack(ICU_getPulseTime);
-	TCCR1B |= (1<<TCCR1B_CS11); //  Start timer1 with 8 prescaler      
+	TIMER2_OVF_setCallBack(ICU_Count_OV);
+	TIMSK |= (1<<TIMSK_TOIE2);
+	TCCR2 |= (1<<TCCR2_CS21); //  Start timer2 with 8 prescaler
 }
 
 void ICU_getPulseTime()
 {
 	
-	static uint16 Falling_edge_time = 0;
+	static uint8 Falling_edge_time = 0;
 	edge_flag++;
 	if (edge_flag == 1)
 	{
-		//TCNT1H = 0;
-		TCNT1L = 0;
+		G_OV_Counts = 0;
+		TCNT2 = 0;
 		INTP0_VidSelectEvent(INT0_FallingEdge);
-	//	edge_flag = 0;
+		//	edge_flag = 0;
 	}
 	else if(edge_flag==2)
 	{
-		Falling_edge_time = TCNT1L;
+		Falling_edge_time = TCNT2;
 
-		G_Pulse_time = (float32)Falling_edge_time;
+		G_Pulse_time = ( (float32)Falling_edge_time )  +  ( (float32)( 255u * G_OV_Counts ) )  ;
+		
 		INTP0_VidSelectEvent(INT0_RisingEdge);
 		edge_flag = 0;
 		G_CallBackFunction();
@@ -48,4 +52,9 @@ void ICU_getPulseTime()
 void ICU_setCallBack(void(*COPY_CallBackFunction)(void))
 {
 	G_CallBackFunction = COPY_CallBackFunction;
+}
+
+void ICU_Count_OV()
+{
+	G_OV_Counts++;
 }
