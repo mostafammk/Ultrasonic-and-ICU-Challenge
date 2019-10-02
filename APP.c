1ba2464da3f9b04b821ff6cf1fdf890ab1cab2f3
minawoww <mina.medhat100@gmail.com>
2019-10-02 16:05:17 +0200

    Update APP.c

--- APP.c
+++ APP.c
@@ -5,7 +5,7 @@
  * Author : minam
  */ 
 
-//#include <avr/io.h>
+
 #define F_CPU 8000000ul
 #include "ICU.h"
 #include "LCD.h"
@@ -14,33 +14,14 @@
 
 int main(void)
 {	
-	ULTRS_init();
-	ICU_init();
-	INTP0_vidEnabled();
-	DIO_Init();
-	LCD_INIT_4bit();
+	SERVICE_init();
 
-	static uint16 L_count=0;
     while (1) 
     {
 		ULTRS_trig();
 		//ULTRS_getDistance();
 		LCD_CLR();
 		LCD_NUM_DISP(0,5,g_Distance);
-		_delay_ms(50);
+		_delay_ms(30);
     }
 }
-
-//
-//if(L_Distance!=g_Distance)
-//{
-	//L_Distance=g_Distance;
-	//ULTRS_trig();
-	//
-	////L_count++;
-//}
-//LCD_CLR();
-//LCD_NUM_DISP(0,5,g_Distance);
-////LCD_String_DISP(0,0,"                ");
-//
-//_delay_ms(10);
