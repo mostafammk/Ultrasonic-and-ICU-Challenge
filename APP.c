a1c7622dea9f3d5b0f432a9ecf148f7791c84ecc
minawoww <mina.medhat100@gmail.com>
2019-10-02 12:55:42 +0200

    dev2

--- APP.c
+++ APP.c
@@ -6,7 +6,7 @@
  */ 
 
 //#include <avr/io.h>
-#define F_CPU 16000000ul
+#define F_CPU 8000000ul
 #include "ICU.h"
 #include "LCD.h"
 #include "ULTRS.h"
@@ -14,28 +14,35 @@
 
 int main(void)
 {	
+	ULTRS_init();
 	ICU_init();
 	INTP0_vidEnabled();
-	ULTRS_init();
+	DIO_Init();
 	LCD_INIT_4bit();
 	volatile float32 freq;
 
-	uint16 L_Distance=g_Distance;
+	//float32 L_Distance=g_Distance;
 	static uint16 L_count=0;
     while (1) 
     {
-		if(L_Distance!=g_Distance)
-		{
-			L_Distance=g_Distance;
-			ULTRS_trig();
-			
-			//L_count++;
-		}
+		ULTRS_trig();
+		//ULTRS_getDistance();
 		LCD_CLR();
 		LCD_NUM_DISP(0,5,g_Distance);
-		//LCD_String_DISP(0,0,"                ");
-		
-		_delay_ms(10);
+		_delay_ms(50);
     }
 }
 
+//
+//if(L_Distance!=g_Distance)
+//{
+	//L_Distance=g_Distance;
+	//ULTRS_trig();
+	//
+	////L_count++;
+//}
+//LCD_CLR();
+//LCD_NUM_DISP(0,5,g_Distance);
+////LCD_String_DISP(0,0,"                ");
+//
+//_delay_ms(10);
