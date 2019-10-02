f8a72eb12333eb7bf6fd58190f9ddbbb91732590
minawoww <mina.medhat100@gmail.com>
2019-10-02 12:52:56 +0200

    dev2

--- ICU.c
+++ ICU.c
@@ -6,7 +6,9 @@
 
 // TIMER0_ov_Count
 
+//float32 G_Pulse_time = 0;
 float32 G_Pulse_time = 0;
+	static uint8 edge_flag = 0;
 void (*G_CallBackFunction)(void) = ( (void*) 0);
 
 
@@ -14,30 +16,29 @@ void ICU_init()
 {
 
 	INTP_vidInit();
-	TCCR1B |= (1<<TCCR1B_CS12) | (1<<TCCR1B_CS10);  //  Start timer1 with 1024 prescaler      
 	INTP0_vidSetCallBack(ICU_getPulseTime);
-	
+	TCCR1B |= (1<<TCCR1B_CS11); //  Start timer1 with 8 prescaler      
 }
 
 void ICU_getPulseTime()
 {
 	
-	static uint16 Rising_edge_time = 0 , Falling_edge_time = 0 , edge_difference = 0;
-	static uint8 edge_flag = 1;
+	static uint16 Falling_edge_time = 0;
+	edge_flag++;
 	if (edge_flag == 1)
 	{
 		//TCNT1H = 0;
 		TCNT1L = 0;
 		INTP0_VidSelectEvent(INT0_FallingEdge);
-		edge_flag = 0;
+	//	edge_flag = 0;
 	}
-	else
+	else if(edge_flag==2)
 	{
 		Falling_edge_time = TCNT1L;
 
-		G_Pulse_time = ( ( (float32)Falling_edge_time * 0.128) /1000.0 );
+		G_Pulse_time = (float32)Falling_edge_time;
 		INTP0_VidSelectEvent(INT0_RisingEdge);
-		edge_flag = 1;
+		edge_flag = 0;
 		G_CallBackFunction();
 		
 	}
